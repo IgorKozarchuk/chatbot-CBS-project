875d260ee090a51000a8a40191d25c3fe8e9eb26
IgorKozarchuk <igor.a.kozarchuk@gmail.com>
2023-07-18 20:34:45 +0300

    jokes

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -2,6 +2,7 @@
 
 
 import random
+import pyjokes
 
 
 # Global vars
@@ -9,12 +10,18 @@ USERNAME = None
 
 
 # Helper functions
+
 def print_bot_line(text):
 	print(f"BART: {text}")
 
 def input_bot_prompt(text):
 	return input(f"BART: {text}")
 
+def print_user_line(text):
+	print(f"{USERNAME}: {text}")
+
+
+# Main functions
 
 def greet():
 	GREETINS = ["Hello", "Hola", "Bonjour", "Buongiorno", "Hi", "Welcome"]
@@ -22,10 +29,46 @@ def greet():
 	welcome_msg = """Greatings! I am BART (Bot Assisting Real Talk) chatbot:)"""
 	print_bot_line(welcome_msg)
 
-	USERNAME = input_bot_prompt("What's your name?\n")
+	global USERNAME
+	USERNAME = input_bot_prompt("What's your name?\n"\
+								f"Username: ")
 
 	print_bot_line(f"{random.choice(GREETINS)}, {USERNAME}!")
 
 
+def select_activity():
+	print_bot_line("What do you want to do?")
+
+	user_choice = input_bot_prompt("Select an activity:\n"\
+									"1 - tell a joke\n"\
+									"2 - recommend a movie\n"\
+									"3 - ...\n"\
+									"0 - EXIT\n"\
+									f"{USERNAME}: "
+	)
+
+	return user_choice
+
+
+def tell_joke():
+	print_bot_line(pyjokes.get_joke())
+
+
+# main loop
+def main_loop():
+	user_choice = None
+
+	while True:
+		user_choice = select_activity()
+		match user_choice:
+			case "1":
+				tell_joke()
+			case "0":
+				break
+			case _:
+				continue
+
+
 if __name__ == "__main__":
 	greet()
+	main_loop()
